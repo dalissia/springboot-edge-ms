084e781645acfe75dfa77bff3478f0c11796a9c2
dalissia <dalissiabenidir@gmail.com>
2024-12-29 15:32:15 +0100

    add client and feign

--- microservice-commandes/src/main/java/com/mcommandes/McommandesApplication.java
+++ microservice-commandes/src/main/java/com/mcommandes/McommandesApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class McommandesApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(McommandesApplication.class, args);
 	}
--- microservice-paiement/src/main/java/com/mpaiement/MpaiementApplication.java
+++ microservice-paiement/src/main/java/com/mpaiement/MpaiementApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MpaiementApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(MpaiementApplication.class, args);
 	}
