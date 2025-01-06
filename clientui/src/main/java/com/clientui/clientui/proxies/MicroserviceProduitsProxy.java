f63848ccbeca979e3415477c8b6c5624036a2f2e
dalissia <dalissiabenidir@gmail.com>
2025-01-06 16:30:17 +0100

    Update clientui config

--- clientui/src/main/java/com/clientui/clientui/proxies/MicroserviceProduitsProxy.java
+++ clientui/src/main/java/com/clientui/clientui/proxies/MicroserviceProduitsProxy.java
@@ -2,14 +2,18 @@ package com.clientui.clientui.proxies;
 
 
 import com.clientui.clientui.beans.ProductBean;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "microservice-produits", url = "localhost:9001")
-public interface MicroserviceProduitsProxy {
+@FeignClient(name = "microservice-produits")
+@RibbonClient(name = "microservice-produits")
+
+public interface MicroserviceProduitsProxy
+{
     @GetMapping(value = "/Produits")
     List<ProductBean> listeDesProduits();
 
