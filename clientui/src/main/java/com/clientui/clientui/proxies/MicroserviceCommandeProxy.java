caadc40deba5b1b0c24633f5e687ccb8abb87185
dalissia <dalissiabenidir@gmail.com>
2025-01-13 14:10:16 +0100

    add proxies

---
+++ clientui/src/main/java/com/clientui/clientui/proxies/MicroserviceCommandeProxy.java
@@ -0,0 +1,15 @@
+package com.clientui.clientui.proxies;
+
+import com.clientui.clientui.beans.CommandeBean;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "zuul-server")
+@RibbonClient(name = "microservice-commandes")
+public interface MicroserviceCommandeProxy {
+
+    @PostMapping(value = "/microservice-commandes/commandes")
+    CommandeBean ajouterCommande(@RequestBody CommandeBean commande);
+}
---
+++ clientui/src/main/java/com/clientui/clientui/proxies/MicroservicePaiementProxy.java
@@ -0,0 +1,18 @@
+package com.clientui.clientui.proxies;
+
+
+import com.clientui.clientui.beans.PaiementBean;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "zuul-server")
+@RibbonClient(name = "microservice-paiement")
+public interface MicroservicePaiementProxy {
+
+    @PostMapping(value = "/microservice-paiement/paiement")
+    ResponseEntity<PaiementBean> payerUneCommande(@RequestBody PaiementBean paiement);
+
+}
